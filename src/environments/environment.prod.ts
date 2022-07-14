a9b7c63ca0dc7707de2ae90fd13c5f01f48e1c80
NeoThe2 <neo.j.jabin@gmail.com>
2022-07-14 18:58:01 +0530

    trying to host

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBDxlFLUqE2WTfJljxUu0YB85h5YuasMd4',
+    authDomain: 'a3-panel-rate-calculator.firebaseapp.com',
+    projectId: 'a3-panel-rate-calculator',
+    storageBucket: 'a3-panel-rate-calculator.appspot.com',
+    messagingSenderId: '1013573475315',
+    appId: '1:1013573475315:web:d99d832946e6b7957f1086',
+    measurementId: 'G-1JECQQWNG7',
+  },
 };
